9e5e869447bc278f55e7e9c8b552aa0552e8324d
Your Name <you@example.com>
2025-05-11 23:54:26 +0200

    konec

--- src/Home.js
+++ src/Home.js
@@ -20,17 +20,17 @@ const Home = () => {
             <div className="second-container">
                 <h2>What is the Pomodoro Technique?</h2>
                 <p className="p-second">
-                    The Pomodoro Technique is a time management method designed to improve focus and productivity. Developed by Francesco Cirillo in the late 1980s, it involves breaking your work into short, focused intervals of typically 25 minutes, known as "Pomodoros," followed by a 5-minute break. After completing four intervals, you take a longer break to recharge. This structured approach helps reduce procrastination, prevent burnout, and make tasks feel more manageable.
+                    The Pomodoro Technique is a time management method designed to improve focus and productivity. Developed by Francesco Cirillo in the late 1980s, it involves breaking your work into short, focused intervals of typically <strong>25 minutes</strong>, known as "Pomodoros," followed by a <strong>5-minute break</strong>. After completing <strong>four intervals</strong>, you take a <strong>longer break</strong> to recharge. This structured approach helps reduce procrastination, prevent burnout, and make tasks feel more manageable.
                 </p>
             </div>
                 <div className="third-container">
                     <h2>How to use PomodoroFlow?</h2>
                     <p className="p-third">
-                        1. Starting the Timer: On the main page, click the Start button to begin the work phase. The timer will start counting down the set time.<br />
-                        2. Pausing and Resuming: You can pause the timer at any time by clicking Pause and resume it by clicking Start again.<br />
-                        3. Switching Between Phases: After the work phase ends, a sound signal will play. Then, manually select the next phase — a short or long break — and start it.<br />
-                        4. Settings: On the Settings page, you can customize the duration of each phase (work, short break, long break), change the app’s color theme, and turn sound effects on or off.<br />
-                        5. Sound Effects: The app will notify you with a sound at the end of each phase. This sound is intended to motivate you to return to work or take a break.
+                        <strong>1. Starting the Timer:</strong> On the main page, click the Start button to begin the work phase. The timer will start counting down the set time.<br />
+                        <strong>2. Pausing and Resuming:</strong> You can pause the timer at any time by clicking Pause and resume it by clicking Start again.<br />
+                        <strong>3. Switching Between Phases:</strong> After the work phase ends, a sound signal will play. Then, manually select the next phase — a short or long break — and start it.<br />
+                        <strong>4. Settings:</strong> On the Settings page, you can customize the duration of each phase (work, short break, long break) and change the app’s color theme.<br />
+                        <strong>5. Sound Effects:</strong> The app will notify you with a sound at the end of each phase. This sound is intended to motivate you to return to work or take a break.
                     </p>
                 </div>
             <div className="fourth-container">
